efda3e313b3bb684733c9fb350839fa8cb5d38af
diego89rps <di_rps@hotmail.com>
2020-06-22 08:05:12 -0300

    Finished color test.

--- BeyondColors/View/ViewController/ColorSecViewController.swift
+++ BeyondColors/View/ViewController/ColorSecViewController.swift
@@ -13,7 +13,13 @@ class ColorSecViewController: UIViewController, Storyboarded {
     let c : ColorsViewModel? = ColorsViewModel()
     var offSet : Int = 0
     var combine : Int = 0
-    var colorComnine : [ColorName] = []
+    var gameState : Int = 0
+    var yellowBtn : Bool = false
+    var orangeBtn : Bool = false
+    var answer : Bool = false
+    let textA = "Escolha as cores para pintar o circulo na cor "
+    let answersList : [Int] = [3, 7, 11, 4, 10, 2, 6, 12, 8]
+    let challengeList : [String] = ["LARANJA", "ROXA", "VERDE", "VERMELHO-ALARANJADO","AZUL-ESVERDEADO","AMARELO-ALARANJADO","VERMELHO-ARROXEADO","AMARELO-ESVERDEADO", "AZUL-ARROXEADO"]
     
     @IBOutlet weak var stackBtnTer: UIStackView!
     @IBOutlet weak var stackBtnSec: UIStackView!
@@ -27,26 +33,29 @@ class ColorSecViewController: UIViewController, Storyboarded {
     @IBOutlet weak var btnYellow: UIButton!
     @IBOutlet weak var btnBlue: UIButton!
     
-    
     @IBOutlet weak var CircleColor: UIView!
-    @IBOutlet weak var instructionsField: UITextView!
+    @IBOutlet weak var instructField: UILabel!
+    @IBOutlet weak var resultAnswer: UILabel!
     
     @IBAction func orangeAction() {
-        changeColor(colorName : .yellow)
+        orangeBtn = true
+        combineTest(value: 3)
     }
     @IBAction func greenAction() {
+        combineTest(value: 11)
     }
     @IBAction func violetAction() {
+        combineTest(value: 7)
     }
-
     @IBAction func redAction() {
-        stackBtnTer.isHidden = false
-        stackDown.constant = 110
-        changeColor(colorName : .blue)
+        combineTest(value: 5)
     }
     @IBAction func yellowAction() {
+        yellowBtn = true
+        combineTest(value: 1)
     }
     @IBAction func blueAction() {
+        combineTest(value: 9)
     }
     
     override func viewDidLoad() {
@@ -57,6 +66,113 @@ class ColorSecViewController: UIViewController, Storyboarded {
         CircleColor.layer.borderWidth = 4
         
         startButtons()
+        
+        let description = changeTextColor(message: NSMutableAttributedString(string: "\(textA)\(challengeList[gameState])"), location: textA.count, length: challengeList[gameState].count, color: (self.c?.createColor(colorBlindnessType: .noType, colorName: .Orange))!)
+        
+        instructField.attributedText = description
+        
+    }
+
+    func combineTest(value: Int){
+        if offSet == 0 {
+            resultAnswer.text = ""
+            combine += value
+            offSet += 1
+            changeColor(colorName : colorResult(value : combine))
+            
+        }
+        else{
+            combine += value
+            yellowBtnTest()
+            orangeBtnTest()
+            combine = combine/2
+            changeColor(colorName : colorResult(value : combine))
+            testAnswer()
+            self.combine = 0
+            self.offSet = 0
+        }
+    }
+    
+    func yellowBtnTest(){
+        if yellowBtn == true && combine > 8{
+            combine += 12
+        }
+        yellowBtn = false
+    }
+    func orangeBtnTest(){
+        if orangeBtn == true && combine == 14{
+            combine = 2
+        }
+        orangeBtn = false
+    }
+    
+    func testAnswer(){
+        if combine == answersList[gameState]{
+            
+            resultAnswer.text = "ACERTOU!"
+            self.gameState += 1
+            if gameState == answersList.count{
+                coordinator?.goToMainViewController()
+            }
+            else{
+                let description = changeTextColor(message: NSMutableAttributedString(string: "\(textA)\(challengeList[gameState])"), location: textA.count, length: challengeList[gameState].count, color: (self.c?.createColor(colorBlindnessType: .noType, colorName: colorResult(value : answersList[gameState])))!)
+                instructField.attributedText = description
+                if gameState == 3 {
+                    stackBtnTer.isHidden = false
+                    stackDown.constant = 110
+                }
+            }
+
+        }
+        else{
+            resultAnswer.text = "ERROU!"
+        }
+    }
+    
+    func colorResult(value : Int) -> ColorName {
+        let color = value
+        switch color {
+            case 1:
+                return ColorName.yellow
+            case 2:
+                return ColorName.yellowOrange
+            case 3:
+                return ColorName.Orange
+            case 4:
+                return ColorName.redOrange
+            case 5:
+                return ColorName.red
+            case 6:
+                return ColorName.redViolet
+            case 7:
+                return ColorName.violet
+            case 8:
+                return ColorName.blueViolet
+            case 9:
+                return ColorName.blue
+            case 10:
+                return ColorName.blueGree
+            case 11:
+                return ColorName.green
+            case 12:
+                return ColorName.yellowGreen
+        default:
+            return ColorName.Orange
+        }
+    }
+    
+    func changeColor(colorName : ColorName){
+        UIView.transition(with: self.CircleColor,
+                          duration: 0.7,
+                          options: .transitionFlipFromTop,
+                          animations: {
+                            self.CircleColor.layer.backgroundColor = self.c?.createColor(colorBlindnessType: .noType, colorName: colorName).cgColor
+        }, completion: nil)
+    }
+    
+    func changeTextColor (message: NSMutableAttributedString, location: Int, length: Int, color: UIColor) -> NSMutableAttributedString {
+        message.addAttribute(NSAttributedString.Key.foregroundColor, value: color, range: NSRange(location: location, length: length))
+        return message
     }
     
     func startButtons(){
@@ -84,14 +200,5 @@ class ColorSecViewController: UIViewController, Storyboarded {
         btnBlue.layer.backgroundColor = c?.createColor(colorBlindnessType: .noType, colorName: .blue).cgColor
         btnBlue.layer.cornerRadius = 10
         btnBlue.layer.borderWidth = 4
-    }
-    
-    func changeColor(colorName : ColorName){
-        UIView.transition(with: self.CircleColor,
-                          duration: 0.7,
-                          options: .transitionFlipFromTop,
-                          animations: {
-                            self.CircleColor.layer.backgroundColor = self.c?.createColor(colorBlindnessType: .noType, colorName: colorName).cgColor
-        }, completion: nil)
     }
 }
